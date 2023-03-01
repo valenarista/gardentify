28206856214709ce8781809bc9a47ccd35544651
Faustino Zanetto <faustizanetto@gmail.com>
2023-03-01 00:08:03 -0300

    feat(dashboard): added dashboard index page

---
+++ apps/dashboard/src/pages/dashboard/index.tsx
@@ -0,0 +1,24 @@
+import { useAuthContext } from '@modules/auth/context/auth-context';
+import Layout from '@modules/layout/components/layout';
+
+const DashbordPage: React.FC = () => {
+  const { state } = useAuthContext();
+
+  return (
+    <Layout
+      headProps={{
+        title: 'Dashboard | Gardentify',
+        description:
+          'Gardentify is a web application that lets you manage and keep tracks of the plants in your garden.',
+      }}
+    >
+      <div className="container mx-auto flex max-w-6xl flex-col space-y-4 px-2 md:px-4 lg:px-6">
+        <div className="rounded-lg bg-neutral-200 p-4 shadow-lg dark:bg-neutral-800">
+          <h1 className="text-start text-2xl font-semibold">👋 Welcome Back, {state.user?.username}</h1>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default DashbordPage;
